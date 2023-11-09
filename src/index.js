32ef11e98caedb3afd985290b9e9c0ecaf9e01d9
notquitelikeme <conquistador8240@gmail.com>
2023-11-09 18:49:45 +0300

    first update

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import { initializeApp } from 'firebase/app'
 import {
     getAuth,
     createUserWithEmailAndPassword,
-    signOut, signInWithEmailAndPassword,
-    FacebookAuthProvider, TwitterAuthProvider,
-    signInWithPopup, onAuthStateChanged
+    signOut, signInWithEmailAndPassword, onAuthStateChanged, updateProfile
 } from 'firebase/auth'
 
 const firebaseConfig = {
@@ -20,9 +18,7 @@ const firebaseConfig = {
 // init firebase app
 initializeApp(firebaseConfig);
 const auth = getAuth();
-const facebookProvider = new FacebookAuthProvider();
 
-const signInFacebook = document.getElementById("signInFacebook");
 const signOutButton = document.getElementById("signOut");
 const message = document.getElementById("message");
 const userName = document.getElementById("userName");
@@ -30,59 +26,8 @@ const userName = document.getElementById("userName");
 signOutButton.style.display = "none";
 message.style.display = "none";
 
-const userSignIn = async () => {
-signInWithPopup(auth, facebookProvider)
-  .then((result) => {
-    const user = result.user;
-    const credential = FacebookAuthProvider.credentialFromResult(result);
-    const accessToken = credential.accessToken;
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    const email = error.customData.email;
-    const credential = FacebookAuthProvider.credentialFromError(error);
-
-  });
-}
-
-const userSignOut = async () => {
-    signOut(auth).then(() => {
-        alert("You have signed out successfully!");
-    }).catch((error) => { })
-}
-
-onAuthStateChanged(auth, (user) => {
-    if (user) {
-        signOutButton.style.display = "block";
-        message.style.display = "block";
-        userName.innerHTML = user.displayName
-    } else {
-        signOutButton.style.display = "none";
-        message.style.display = "none";
-    }
-})
-
-signInFacebook.addEventListener('click', userSignIn);
-signOutButton.addEventListener('click', userSignOut);
-
 // signing users up
-const signupForm = document.querySelector('.signup')
-signupForm.addEventListener('submit', (e) => {
-    e.preventDefault()
 
-    const email = signupForm.email.value
-    const password = signupForm.password.value
-
-    createUserWithEmailAndPassword(auth, email, password)
-        .then((cred) => {
-            console.log('user created:', cred.user)
-            signupForm.reset()
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
 
 // logging in and out
 const logoutButton = document.querySelector('.logout')
